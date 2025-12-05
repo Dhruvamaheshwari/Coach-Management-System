29cd890c097364cee2ddf4194b34df1da88d5817
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2025-12-06 01:12:36 +0530

    login route is ready only left with the auth part structure is ready

--- Backend/Controller/AuthUser.js
+++ Backend/Controller/AuthUser.js
@@ -2,21 +2,45 @@
 // import the user.js from model
 const User = require('../model/user');
 
-exports.singup = async(req , res) =>{
-    try{
+
+// This is for the Singup page;
+//todo -> Singup is done only left is the jwt and password hash
+exports.singup = async (req, res) => {
+    try {
         // get data;
-        const {first_name , last_name , email , password , role} = req.body;
+        const { first_name, last_name, email, password, role } = req.body;
 
         // to check user is already exist or not
         const userexist = await User.findOne({ email });
-        
-        if(userexist)
-            return res.status(400).json({succ:false ,mess:'user is already singup pls login'})
 
-        const userCreate = await User.create({first_name , last_name , email , password , role});
-            return res.status(200).json({succ:true , user:userCreate , mess:'user is created'});
-    }catch(err){
+        if (userexist)
+            return res.status(400).json({ succ: false, mess: 'user is already singup pls login' })
+
+        const userCreate = await User.create({ first_name, last_name, email, password, role });
+        return res.status(200).json({ succ: true, user: userCreate, mess: 'user is created' });
+    } catch (err) {
         console.log(err)
-        return res.status(500).json({mess: err , suss:false });
+        return res.status(500).json({ mess: err, suss: false });
     }
 }
+
+// This is for the Login page
+//todo -> login is done only auth is left
+exports.login = async (req, res) => {
+    try {
+        // get the data
+        const { email, password } = req.body;
+
+        // to check email is presnt or not in the database
+        const userEnter = await User.findOne({ email });
+
+        if (!userEnter)
+            return res.status(400).json({ succ: false, mess: "you are not loign pls login first" })
+
+        return res.status(200).json({ succ: true, mess: "user is login successfully", user: userEnter })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ succ: false, mess: err })
+    }
+}
+
--- Backend/route/UserRouter.js
+++ Backend/route/UserRouter.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 
 // import the AuthUser.js from the Controller
-const {singup} = require("../Controller/AuthUser");
+const {singup , login} = require("../Controller/AuthUser");
 
 // to create the route
 router.post("/singup" , singup);
+router.post("/login" , login)
 
 
 
